13beb794e3b9b3f20758817cced8a201c3ba4e8a
Usman Kapadiya <hirenk@intellidt.com>
2024-10-07 18:16:27 +0530

    Company history and products details screen set

---
+++ companyHistory.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS Metal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        
+        .companyHistory-banner {
+            background-image: url('./img/CompanyHistory/banner.webp');
+                background-size: cover;
+                background-position: center;
+                transition: background-size 0.2s ease; 
+                width: 100%;
+            }
+    
+            .banner-container {
+                height: 45vh !important; 
+                overflow: hidden; 
+            }
+    </style>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <div class="container">
+        <div class="line line-left"></div>
+        <div class="line line-1-3"></div>
+        <div class="line line-2-3"></div>
+        <div class="line line-right"></div>
+        <div class="row section-padding">
+            <div class="col-sm-8">
+                <h5><span class="text-color">/ </span>people</h5>
+                <h1 class="display-4">Embracing History, Forging Tomorrow.</h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- banner -->
+    <div class="banner-container companyHistory-banner"> 
+            <div class="container">
+                <div class="row">
+                    <a class="col-sm-4 image-button-white" href="">
+                        <div class="d-flex justify-content-between w-100">
+                            <h5 class="mb-0">LEARN MORE</h5>
+                            <h5 class="mb-0">↘</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    <div class="container">
+        <div class="line line-left"></div>
+        <div class="line line-1-3"></div>
+        <div class="line line-2-3"></div>
+        <div class="line line-right"></div>
+
+
+        <!-- Intro Section -->
+        <div class="row section-padding">
+            <div class="col-sm-12 col-md-4">
+                <h4>From Roots to Horizons: Our Steel Journey</h4>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <p>For over a century, Fabrik has proudly led the industry, taking on the responsibility of envisioning what lies ahead. Driven by advanced technology and innovative thinking, we anticipate the future needs of our customers and the world.</p>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <p>
+                    Our mission extends beyond steel; it encompasses safeguarding lives and livelihoods. Every day, we immerse ourselves in purposeful work, addressing the critical tasks that demand our attention.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="line line-left"></div>
+        <div class="line line-1-2"></div>
+        <div class="line line-right"></div>
+        <div class="border"></div>
+        <div class="container">
+            <div class="row section-paddin pt-5">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <h4>1996</h3>
+                        <h5>Formation of Akita Systems</h5>
+                        <p>The company is founded and begins building POs for
+                            local businesses out of a spare bedroom.</p>
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <h4>1998</h3>
+                        <h5>First office in Sevenoaks</h5>
+                        <p>It's only a portable cabin but at least we're out of the bedroom!</p>
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <h4>2000</h3>
+                        <h5>Beginning of IT support services</h5>
+                        <p>Focus switches from hardware supply to IT support.</p>
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <h4>2003</h3>
+                        <h5>Move to Chaucer Business Park</h5>
+                        <p>A move from the Sevenoaks cabin to renting in nearby Kemsing.</p>
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <h4>2009</h3>
+                        <h5>Cloud computing</h5>
+                        <p>Development of our cloud solutions with launch of our
+                            Maidstone datacentre presence.</p>
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <h4>2012</h3>
+                        <h5>Rebrand</h5>
+                        <p>The company tales a significant shift forward with its appearance.</p>
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <h4>2013</h3>
+                        <h5>Start of 24/7 support</h5>
+                        <p>We begin to offer enhanced support services for customers operating outside of standard hours.</p>
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <h4>2014</h3>
+                        <h5>Cloud Expansion</h5>
+                        <p>We expand our cloud infrastructure with additional data centre capacity in Reading.</p>
+                </div>
+            </div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-6" style="text-align: end; padding:0% 7% 0% 14%;">
+                    <h4>2016</h3>
+                        <h5>Arrival at Nepicar Park - Our Kent HQ</h5>
+                        <p>Growth demands new premises and Akita moves into
+                            its current Kent HQ.</p>
+                </div>
+                <div class="col-sm-12 col-md-6" style="padding: 0% 14% 0% 7%;">
+                    <img src="img/Product/product-2.webp" alt="product 3" style="width: 50%;">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <?php include 'footer.php'; ?>
+
+    <script>
+            window.addEventListener('scroll', function() {
+                const banner = document.querySelector('.companyHistory-banner');
+                const scrollPosition = window.scrollY;
+
+                // Calculate zoom level based on scroll position
+                const zoomLevel = 1 + (scrollPosition / 1000); // Adjust divisor to control zoom speed
+
+                // Limit the maximum zoom level to prevent excessive zoom
+                const maxZoomLevel = 1.1; // Set a maximum zoom level (e.g., 10% zoom)
+                banner.style.backgroundSize = `${Math.min(zoomLevel, maxZoomLevel) * 100}%`; // Set background size
+            });
+        </script>
+</body>
+
+</html>
---
+++ productsDetail.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS Metal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+     <style>
+        
+        .productsDetails-banner {
+            background-image: url('./img/ProductsDetail/banner.webp');
+                background-size: cover;
+                background-position: center;
+                transition: background-size 0.2s ease; 
+                width: 100%;
+            }
+    
+            .banner-container {
+                height: 45vh !important; 
+                overflow: hidden; 
+            }
+    </style>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <div class="container">
+        <div class="line line-left"></div>
+        <div class="line line-1-3"></div>
+        <div class="line line-2-3"></div>
+        <div class="line line-right"></div>
+        <div class="row section-padding">
+            <div class="col-sm-8">
+                <h5><span class="text-color">/ </span>Diversity & Inclusion</h5>
+                <h1 class="display-4">Diverse talent enables smarter steels</h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- banner -->
+    <div class="banner-container productsDetails-banner">
+        <div class="container">
+            <div class="row">
+                <a class="col-sm-4 image-button-white" href="">
+                    <div class="d-flex justify-content-between w-100">
+                        <h5 class="mb-0">LEARN MORE</h5>
+                        <h5 class="mb-0">↘</h5>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="line line-left"></div>
+        <div class="line line-right"></div>
+        <!-- Intro Section -->
+        <div class="section-padding">
+            <div style="padding: 5% 15% 5% 15%;">
+                <h5><span class="text-color">/ </span>Talented and Motivated Workforce</h5>
+                <h4>Fabrik’s mission centers on crafting intelligent steels that benefit both humanity and the planet. Achieving this ambition hinges on attracting and retaining top-tier talent. Our success in this endeavor rests on fostering an inclusive workplace where every team member feels esteemed and empowered. </h4>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row p-3">
+                <div class="col-sm-12 col-md-6 section-padding">
+                    <img src="img/Product/product-2.webp" alt="product 3" width="100%">
+                </div>
+                <div class="col-sm-12 col-md-6" style="background-color: black;">
+                    <div class="text-white" style="padding: 15%;">
+                        <h1>STAMPING PRODUCTS</h1>
+                        <div class="row section-padding" style="line-height: 3.5;">
+                            <div class="col-sm-12 col-md-6">
+                                <div>Hinges</div>
+                                <div>Brackets</div>
+                                <div>Wire Mesh</div>
+                                <div>Hood Fan Accessory</div>
+                                <div>Stamping Parts</div>
+                                <div>Aluminnum Stamping Parts</div>
+                                <div>Chassis Atamping</div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div>Stamping Panels</div>
+                                <div>Rings</div>
+                                <div>Stamping Cooktop</div>
+                                <div>Stamping Bowl</div>
+                                <div>Stamping Vent Trim</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container pt-5">
+                <div class="row section-padding pl-0 pr-0">
+                    <div class="col-sm-12 col-md-6">
+                        <img src="img/ProductsDetail/productsdetails1.webp" alt="image 2" style="width: 100%;">
+                    </div>
+                    <div class="col-sm-12 col-md-6 d-flex align-items-center">
+                        <div class="p-5">
+                            <h5><span class="text-color">/ </span>Become a customer</h5>
+                            <h3 class="mt-5 mb-5">LEADING THE WAY TO THE FUTURE</h3>
+                            <p>Fabrik blends steel technology leadership with outstanding problem-solving capabilities to support customers and suppliers in shaping the future of automotive transportation.</p>
+                            <a href="" class="underline-link">LET'S TALK →</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <?php include 'footer.php'; ?>
+
+    <script>
+            window.addEventListener('scroll', function() {
+                const banner = document.querySelector('.productsDetails-banner');
+                const scrollPosition = window.scrollY;
+
+                // Calculate zoom level based on scroll position
+                const zoomLevel = 1 + (scrollPosition / 1000); // Adjust divisor to control zoom speed
+
+                // Limit the maximum zoom level to prevent excessive zoom
+                const maxZoomLevel = 1.1; // Set a maximum zoom level (e.g., 10% zoom)
+                banner.style.backgroundSize = `${Math.min(zoomLevel, maxZoomLevel) * 100}%`; // Set background size
+            });
+        </script>
+</body>
+
+</html>
