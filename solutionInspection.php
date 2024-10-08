6e1659affa604fe6432e15168ef5e89ca0999255
Usman Kapadiya <hirenk@intellidt.com>
2024-10-08 16:10:52 +0530

    Solutions inspection screen create

---
+++ solutionInspection.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CS Metal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="./js/main.js"></script>
+    <!-- <link rel="apple-touch-icon" sizes="180x180" href="./img/icons/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./img/icons/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./img/icons/favicon/favicon-16x16.png">
+        <link rel="manifest" href="./img/icons/favicon/site.webmanifest"> -->
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+    <div class="container">
+        <div class="line line-left"></div>
+        <div class="line line-2-3"></div>
+        <div class="line line-right"></div>
+        <div class="row section-padding">
+            <div class="col-sm-8">
+                <h5><span class="text-color">/ </span>Solutions</h5>
+                <h1 class="display-4">Appliances</h1>
+            </div>
+        </div>
+        <div class="row" style="background-color: #eee; position:relative">
+            <div class="col-md-8">
+                <h4 style="padding: 20% 17% 15% 5%;">Fabrik is crucial in the USA’s emergence as a favored global auto manufacturing destination. We focus on lightweight, cost-effective, and fuel-efficient vehicle solutions, collaborating closely with the automotive sector from concept to commercialization. Our experience, expertise, and innovation consistently exceed our clients' high QCD (Quality, Cost, and Delivery) expectations.</h4>
+            </div>
+            <div class="col-md-4" href="#">
+                <img src="./img/SolutionInspection/banner.webp" alt="Home Banner" width="100%" style="position:absolute; bottom:10%; right: 12%;">
+            </div>
+        </div>
+
+
+
+        <div class="container" style="border-bottom:1px solid #eee;">
+            <div class="line line-left"></div>
+            <div class="line line-1-3"></div>
+            <div class="line line-2-3"></div>
+            <div class="line line-right"></div>
+
+            <div class="section-padding">
+                <h5><span class="text-color">/ </span>GOM SCAN</h5>
+                <div class="row pt-5">
+                    <div class="col-sm-12 col-md-4">
+                        <h1>3D SCANNING TECHNOLOGY</h1>
+                    </div>
+                </div>
+                <div class="row pt-5">
+                    <div class="col-sm-12 col-md-4">
+                        <div>
+                            <p>Welcome to our introduction to Gom Scan, a leading solution in 3D scanning technology, specifically designed for precision in metal stamping parts. With Gom Scan, we ensure the highest quality throughv rigorous first piece inspections, enabling us to detect any deviations from specifications right from the start.
+                            </p>
+                            <P>As we transition into mass production, Gom Scan plays a crucial role in maintaining consistent quality control (QC), allowing for real-time monitoring and adjustment throughout the manufacturing process. This not only enhances the reliability of our products but also drives process improvement initiatives by identifying areas for optimization.
+                            </P>
+                            <P>Join us as we explore how Gom Scan is revolutionizing the metal stamping industry, ensuring accuracy and efficiency at every stage. from the tirst inspection to large-scale production!</P>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12 col-md-4 product-margin">
+                        <div class="product-wrap pt-5">
+                            <div class="pt-5">
+                                <img src="img/SolutionInspection/solution3.webp" alt="product 2" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12 col-md-4">
+                        <div class="product-wrap pt-5 mt-5">
+                            <div class="pt-5 mt-5">
+                                <img src="img/SolutionInspection/solution2.webp" alt="product 2" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" style="border-bottom:1px solid #eee;">
+            <div>
+                <div class="text-center">
+                    <h1 class="py-5">PRECISE DIMENSIONAL MEASUREMENT</h1>
+                    <iframe width="100%" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+                    </iframe>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="line line-left"></div>
+            <div class="line line-1-3"></div>
+            <div class="line line-2-3"></div>
+            <div class="line line-right"></div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-4">
+                    <p>Introducing our advanced Coordinate Measuring Machine (CMM), Inspector, a state-of-the-art measuring device designed for precise dimensional inspection in manufacturing. This innovative tool features computerized movement along the X. Y, and Z axis, allowing for highly accurate measurements of both large and small inner and outer dimensions.</p>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <p>The CMM Inspector is perfect for a variety of applications, delivering reliple results across different sizes and snapes of comporients. Its ability to generate detailed digital reports streamlines the quality assurance process, providing manufacturers with clear documentation of measurement results for easy analysis and compliance tracking</p>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <p>With its advanced technology and user-friendly interface, our CMM Inspector enhances productivity and ensures the highest standards of accuracy in dimensional measurement. Experience the benefits of precision measurement with our CMM Inspector, a crucial asset for maintaining product quality and integrity in any manufacturing environment!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="background-color:black;">
+        <div class="container">
+            <div class="row text-white" style="padding:8% 0%">
+                <div class="col-md-6">
+                    <h5><span class="text-color">/ </span>Spectralight QC Light Booth</h5>
+                    <h1>COLOR QUALITY CONTROL</h1>
+                </div>
+                <div class="col-md-6 ">
+                    <p>Introducing the Spectralight OC Light Booth, a state-of-the-art quality control solution designed to ensure accurate color judgment in aesthetic components. A key feature of this booth is its use of controlled light temperature along with standardized light sources, including D65, CWF, and A light tealleratures, which are essential for simulating natural daylight and various artificial lighting conditions.</p>
+                </div>
+            </div>
+            <div>
+                <img src="img/SolutionInspection/solution4.webp" alt="" width="100%">
+            </div>
+        </div>
+    </div>
+    <div class="container" style="border-bottom:1px solid #eee;">
+        <div>
+            <div class="text-center">
+                <h1 class="py-5">HIGH STANDARDS OF QUALITY & ACCOUNTABILITY</h1>
+                <iframe width="100%" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+                </iframe>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="line line-left"></div>
+
+        <div class="line line-2-3"></div>
+        <div class="line line-right"></div>
+        <div class="row section-padding">
+            <div class="col-sm-12 col-md-4">
+                <p>The CS Metal Quality Management System (OMS) is a customized software solution designed to ensure consistent quality throughout the manufacturing process. At the core of this OMS is a paperless quality record database that streamlines data management and enhances operational efficiency.</p>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <p>Quality data is input dally from various stages, including incoming materials inspections, in-process evaluations, critical-to-quality control charts, and finished goods inspections, This sophisticated software allows for efficient data entry and real-time quality monitoring, enabling timely identification of any issues that may arise during production.</p>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <p>The system facilitates comprehensive quality performance management reviews, providing insights into trends and areas for improvement. Additionally, the OMS ensures quality traceability, allowing for easy tracking of materials and components throughout the manufacturing process.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+    <?php include 'footer.php'; ?>
+
+    <script>
+        // nut drop-down effect
+        window.addEventListener('scroll', function() {
+            const svgWrappers = document.querySelectorAll('.nut-wrapper');
+            svgWrappers.forEach(wrapper => {
+                const rect = wrapper.getBoundingClientRect();
+                const windowHeight = window.innerHeight;
+
+                if (rect.top < windowHeight - 100) {
+                    wrapper.classList.add('visible');
+                } else {
+                    wrapper.classList.remove('visible');
+                }
+            });
+        });
+
+        // zoom in image when scrolling
+        // window.addEventListener('scroll', function() {
+        //     const image = document.querySelector('.banner-container');
+        //     const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+        //     const scrollPercentage = window.scrollY / maxScroll;
+
+        //     const scale = 1 + scrollPercentage * 0.3;
+        //     image.style.transform = `scale(${scale})`;
+        // });
+    </script>
+</body>
+
+</html>
