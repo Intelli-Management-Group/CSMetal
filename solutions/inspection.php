fc3b167cce60ef87ad0bb3e04aa7a8317bececc7
TonyHuang <hzx0109@gmail.com>
2024-10-11 12:26:06 -0700

    update inspection page

--- solutions/inspection.php
+++ solutions/inspection.php
@@ -2,23 +2,7 @@
 <html lang="en">
 
 <head>
-    <!-- <meta charset="utf-8">
-    <title>CS Metal</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script> -->
-    <!-- <link rel="apple-touch-icon" sizes="180x180" href="./img/icons/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./img/icons/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./img/icons/favicon/favicon-16x16.png">
-    <link rel="manifest" href="./img/icons/favicon/site.webmanifest"> -->
     <?php include '../HTMLhead.php'; ?>
-    <style>
-
-    </style>
 </head>
 
 <body>
@@ -40,19 +24,18 @@
                 <h4 class="solution-banner-padding">Fabrik is crucial in the USA’s emergence as a favored global auto manufacturing destination. We focus on lightweight, cost-effective, and fuel-efficient vehicle solutions, collaborating closely with the automotive sector from concept to commercialization. Our experience, expertise, and innovation consistently exceed our clients' high QCD (Quality, Cost, and Delivery) expectations.</h4>
             </div>
             <div class="col-md-4">
-                <img src="img/SolutionInspection/banner.webp" alt="Inspection Banner" width="100%" class="solution-banner scroll2moveup">
+                <img src="img/SolutionInspection/banner.webp" alt="Inspection Banner" class="image solution-banner scroll2moveup">
             </div>
         </div>
     </div>
 
-    <!-- GOM Scan -->
+    <!-- GOM Scan Section -->
     <div class="container border-bottom">
         <div class="line line-left"></div>
         <div class="line line-1-3"></div>
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
 
-        <!-- GOM Scan Section -->
         <div class="section-padding">
             <h5><span class="text-color">/ </span>GOM SCAN</h5>
             <div class="row">
@@ -69,11 +52,11 @@
                     </div>
                 </div>
 
-                <div class="col-sm-12 col-md-4" style="margin-top: 200px">
-                    <img src="img/SolutionInspection/solution2.webp" alt="product 2" class="image scroll2movedown">
+                <div class="col-sm-12 col-md-4 solution-image-1">
+                    <img src="img/SolutionInspection/solution2.webp" alt="product 1" class="image scroll2movedown">
                 </div>
 
-                <div class="col-sm-12 col-md-4" style="margin-top: 400px">
+                <div class="col-sm-12 col-md-4 solution-image-2">
                     <img src="img/SolutionInspection/solution3.webp" alt="product 2" class="image scroll2moveup">
                 </div>
             </div>
@@ -84,14 +67,14 @@
     <div class="container">
         <div class="line line-left"></div>
         <div class="line line-right"></div>
-        <div>
-            <div class="text-center">
-                <h1 class="py-5">PRECISE DIMENSIONAL MEASUREMENT</h1>
-                <iframe width="100%" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY?mute=1">
-                </iframe>
+        <div class="text-center">
+            <h1 class="m-0 section-padding">PRECISE DIMENSIONAL MEASUREMENT</h1>
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY?mute=1"></iframe>
             </div>
         </div>
     </div>
+
     <div class="container">
         <div class="line line-left"></div>
         <div class="line line-1-3"></div>
@@ -99,10 +82,10 @@
         <div class="line line-right"></div>
         <div class="row section-padding">
             <div class="col-sm-12 col-md-4">
-                <p>Introducing our advanced Coordinate Measuring Machine (CMM), Inspector, a state-of-the-art measuring device designed for precise dimensional inspection in manufacturing. This innovative tool features computerized movement along the X. Y, and Z axis, allowing for highly accurate measurements of both large and small inner and outer dimensions.</p>
+                <p>Introducing our advanced Coordinate Measuring Machine (CMM), Inspector, a state-of-the-art measuring device designed for precise dimensional inspection in manufacturing. This innovative tool features computerized movement along the X, Y, and Z axis, allowing for highly accurate measurements of both large and small inner and outer dimensions.</p>
             </div>
             <div class="col-sm-12 col-md-4">
-                <p>The CMM Inspector is perfect for a variety of applications, delivering reliple results across different sizes and snapes of comporients. Its ability to generate detailed digital reports streamlines the quality assurance process, providing manufacturers with clear documentation of measurement results for easy analysis and compliance tracking</p>
+                <p>The CMM Inspector is perfect for a variety of applications, delivering reliple results across different sizes and snapes of comporients. Its ability to generate detailed digital reports streamlines the quality assurance process, providing manufacturers with clear documentation of measurement results for easy analysis and compliance tracking.</p>
             </div>
             <div class="col-sm-12 col-md-4">
                 <p>With its advanced technology and user-friendly interface, our CMM Inspector enhances productivity and ensures the highest standards of accuracy in dimensional measurement. Experience the benefits of precision measurement with our CMM Inspector, a crucial asset for maintaining product quality and integrity in any manufacturing environment!</p>
@@ -111,36 +94,57 @@
     </div>
 
     <!-- Spectralight Section -->
-    <div style="background-color:black;">
+    <div class="black-white-background">
         <div class="container">
-            <div class="row text-white" style="padding:8% 0%">
+            <div class="row section-padding text-white">
                 <div class="col-md-6">
                     <h5><span class="text-color">/ </span>Spectralight QC Light Booth</h5>
                     <h1>COLOR QUALITY CONTROL</h1>
                 </div>
                 <div class="col-md-6 ">
-                    <p>Introducing the Spectralight OC Light Booth, a state-of-the-art quality control solution designed to ensure accurate color judgment in aesthetic components. A key feature of this booth is its use of controlled light temperature along with standardized light sources, including D65, CWF, and A light tealleratures, which are essential for simulating natural daylight and various artificial lighting conditions.</p>
+                    <p>Introducing the Spectralight QC Light Booth, a state-of-the-art quality control solution designed to ensure accurate color judgment in aesthetic components. A key feature of this booth is its use of controlled light temperature along with standardized light sources, including D65, CWF, and A light tealleratures, which are essential for simulating natural daylight and various artificial lighting conditions.</p>
                 </div>
             </div>
-            <div>
-                <img src="img/SolutionInspection/solution4.webp" alt="" width="100%">
+
+            <div class="image-container">
+                <img src="img/SolutionInspection/solution4.webp" alt="Spectralight" class="banner-container">
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="line line-left"></div>
+            <div class="line line-right"></div>
+            <div class="line line-1-3"></div>
+            <div class="line line-2-3"></div>
+            <div class="row section-padding">
+                <div class="col-sm-12 col-md-4">
+                    <p>The Spectralight QC Light Booth incorporates controlled light temperature management for optimal viewing conditions, ensuring consistency and reliability in color assessment. This controlled environment minimizes the impact of external light variables that can skew color perception, allowing for precise evaluation.</p>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <p>We have a single light compartment dedicated to inspecting small components to facilitate detailed analyses. For larger components and high-volume batches of colored products, a duo light booth design is provided to accommodate comprehensive evaluations without sacrificing accuracy. </p>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <p>The primary objective of the Spectralight QC Light Booth is to evaluate a reproduction of appearance parts by comparing samples against a master target. This process enables precise detection of any color variance, empowering manufacturers to maintain high-quality standards and reduce the risk of defects. By incorporating this advanced inspection technology into your manufacturing process, you can significantly enhance product consistency, quality assurance, and ultimately, customer satisfaction. Experience the exceptional precision and reliability of the Spectralight QC Light Booth—an essential tool for any organization committed to excellence in color quality control! </p>
+                </div>
             </div>
         </div>
     </div>
-    <div class="container">
+
+    <!-- Quality Management System -->
+    <div class="container border-top">
         <div class="line line-left"></div>
         <div class="line line-right"></div>
-        <div>
-            <div class="text-center">
-                <h1 class="py-5">HIGH STANDARDS OF QUALITY & ACCOUNTABILITY</h1>
-                <iframe width="100%" height="500" src="https://www.youtube.com/embed/tgbNymZ7vqY?mute=1">
-                </iframe>
+        <div class="text-center">
+            <h1 class="m-0 section-padding">HIGH STANDARDS OF QUALITY & ACCOUNTABILITY</h1>
+            <div class="video-container">
+                <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY?mute=1"></iframe>
             </div>
         </div>
     </div>
+
     <div class="container">
         <div class="line line-left"></div>
-
+        <div class="line line-1-3"></div>
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding">
@@ -155,37 +159,9 @@
             </div>
         </div>
     </div>
-    </div>
-
 
     <?php include '../footer.php'; ?>
 
-    <script>
-        // nut drop-down effect
-        // window.addEventListener('scroll', function() {
-        //     const svgWrappers = document.querySelectorAll('.nut-wrapper');
-        //     svgWrappers.forEach(wrapper => {
-        //         const rect = wrapper.getBoundingClientRect();
-        //         const windowHeight = window.innerHeight;
-
-        //         if (rect.top < windowHeight - 100) {
-        //             wrapper.classList.add('visible');
-        //         } else {
-        //             wrapper.classList.remove('visible');
-        //         }
-        //     });
-        // });
-
-        // zoom in image when scrolling
-        // window.addEventListener('scroll', function() {
-        //     const image = document.querySelector('.banner-container');
-        //     const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-        //     const scrollPercentage = window.scrollY / maxScroll;
-
-        //     const scale = 1 + scrollPercentage * 0.3;
-        //     image.style.transform = `scale(${scale})`;
-        // });
-    </script>
 </body>
 
 </html>
