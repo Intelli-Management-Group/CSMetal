56c3983c3e13baa824da2dc284f3f5560dfdfadf
TonyHuang <hzx0109@gmail.com>
2024-12-17 15:17:23 -0800

    update responsive footer

--- inc/footer.php
+++ inc/footer.php
@@ -12,13 +12,19 @@
                 color: white;
             }
 
-            .info-wrap {
+            /* .info-wrap {
                 display: flex;
+                flex-direction: column;
                 align-items: center;
-                /* justify-content: center; */
+                justify-content: center;
                 text-align: left;
                 box-sizing: border-box;
-                padding: 15px;
+                padding: 0 15px;
+            } */
+
+            .info-wrap a {
+                color: white;
+                text-decoration: none;
             }
 
             nav {
@@ -31,22 +37,36 @@
                 align-items: center;
                 margin: 0;
                 padding: 0;
+                flex-wrap: wrap;
             }
 
-            .terms li {
+            /* .terms li {
                 margin-left: 20px;
             }
+
+            @media  only screen and (max-width: 470px) {
+                .terms li {
+                    margin-left: 20px;
+                }
+            } */
 
             .terms a {
                 color: white;
                 text-decoration: none;
                 font-size: 0.8em !important;
                 font-weight: 300 !important;
-                padding: 5px 10px;
+                /* padding: 5px 10px; */
             }
 
             .terms a.active {
                 color: #0c73b2;
+            }
+
+            .terms li:first-child {
+                width: 100%;
+            }
+            .terms li:nth-child(3) {
+                padding-left: 10px;
             }
 
             /* .terms a:hover {
@@ -86,15 +106,15 @@
                                 <h3 class="mb-0">Redefining The Standards in Metal Manufacturing</h3>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-3 col-lg-3 p-0">
+                        <div class="col-sm-12 col-md-3 col-lg-4 p-0">
                             <div class="info-wrap email">
                                 <p class="mb-0">2A/F, Phase 1 Kingsford Industrial Bldg., <br>26-32 Kwai Hei St., Kwai Chung Hong Kong</p>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-3 col-lg-3 p-0">
+                        <div class="col-sm-12 col-md-3 col-lg-2 pl-lg-0 pl-md-3 p-0">
                             <div class="info-wrap phone">
-                                <p class="mb-0">factory@chising.com.hk <br>
-                                   +852-2614-3700</p>
+                                <a href="tel:+85226143700">+852-2614-3700</a><br>
+                                <a href="mailto:factory@chising.com.hk">factory@chising.com.hk</a>
                             </div>
                         </div>
                     </div>
@@ -104,14 +124,18 @@
             <hr class="m-0" style="background-color: #4b4b4b;">
 
             <div class="container">
-                <div class="footer section-padding pl-0">
-                    <nav>
-                        <ul class="terms">
-                            <li class="ml-0">© <?php echo Date('Y'); ?> CS Metal</li>
-                            <li><a href="#">Terms & Conditions</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                        </ul>
-                    </nav>
+                <div class="footer section-padding">
+                    <div class="row">
+                        <div class="col-12 p-md-0">
+                            <nav>
+                                <ul class="terms">
+                                    <li class="ml-0">© <?php echo Date('Y'); ?> CS Metal</li>
+                                    <li><a href="#">Terms & Conditions</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -125,11 +149,31 @@
             const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
             // Show button when user scrolls down 100px from top
+            // window.onscroll = function() {
+            //     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            //         scrollToTopBtn.style.display = "block";
+            //     } else {
+            //         scrollToTopBtn.style.display = "none";
+            //     }
+            // };
+
             window.onscroll = function() {
-                if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+                const windowHeight = document.documentElement.clientHeight;
+                const documentHeight = document.documentElement.scrollHeight;
+
+                // Show button when scrolled down 100px from top
+                if (scrollPosition > 100) {
                     scrollToTopBtn.style.display = "block";
                 } else {
                     scrollToTopBtn.style.display = "none";
+                }
+
+                // Hide button near the bottom
+                if (window.innerWidth < 576) {
+                    if (scrollPosition + windowHeight >= documentHeight - 10) {
+                        scrollToTopBtn.style.display = "none";
+                    }
                 }
             };
 
