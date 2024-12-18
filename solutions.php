82b26eb1511d6e2747661d2ef96e3ab2b5b38cdd
TonyHuang <hzx0109@gmail.com>
2024-12-18 14:22:04 -0800

    update responsive for solution page

--- solutions.php
+++ solutions.php
@@ -12,7 +12,7 @@
             <div class="line line-2-3"></div>
             <div class="line line-right"></div>
             <div class="row section-padding">
-                <div class="col-md-8">
+                <div class="col-md-8 py-0">
                     <p class="slash-title"><span class="text-color">/ </span>Solutions</p>
                     <div class="title">Precision-Driven Metal Assurance</div>
                 </div>
@@ -40,27 +40,27 @@
             <div class="line line-right"></div>
 
             <div class="row section-padding" id="intro">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pt-0">
                     <p class="slogan mb-0">DELIVERING COMPREHENSIVE SOLUTIONS WITH UNMATCHED QUALITY AND RELIABILITY </p>
                 </div>
                 <div class="col-sm-12 col-md-4">
                     <p class="mb-0">Manufacturers in metal production require robust, precise solutions to uphold quality and ensure product durability across every stage of production. From initial hardness testing to transportation simulations, our advanced tools and technologies safeguard product integrity, allowing companies to meet the highest standards in material performance and reliability.</p>
                 </div>
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pb-0">
                     <p class="mb-0">At CS Metal, our engineering and testing teams collaborate closely with manufacturers to offer a comprehensive suite of testing solutions. We are committed to precision-driven, quality-assured products through innovative processes that optimize production and reduce waste, helping our customers achieve excellence in every detail.</p>
                 </div>
             </div>
 
             <!-- Company Profile -->
             <div class="row section-padding">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pt-0">
                     <div class="product-wrap">
                         <div class="product-image-wrap">
                             <img src="./img/Solution/img_inspection.jpg" alt="product 1" style="width: 100%;">
                             <a class="image-arrow" href="solutions/inspection.php"><div>→</div></a>
                         </div>
                         <h3 class="mt-3">Inspection</h3>
-                        <p>CS Metal upholds top-quality standards with advanced inspection tools: Gom Scan for early defect detection, Inspector CMM for precise measurements, Spectralight QC for color accuracy, and QMS software for real-time tracking. These technologies ensure reliability from inspection to production, highlighting our commitment to quality in every aspect of manufacturing.</p>
+                        <p class="mb-0">CS Metal upholds top-quality standards with advanced inspection tools: Gom Scan for early defect detection, Inspector CMM for precise measurements, Spectralight QC for color accuracy, and QMS software for real-time tracking. These technologies ensure reliability from inspection to production, highlighting our commitment to quality in every aspect of manufacturing.</p>
                     </div>
                 </div>
 
@@ -71,18 +71,18 @@
                             <a class="image-arrow" href="solutions/machine.php"><div>→</div></a>
                         </div>
                         <h3 class="mt-3">Machines</h3>
-                        <p>CS Metal’s advanced machines enhance manufacturing precision: the Automated Tube Forming Machine uses AI for real-time control, the 600 Ton Stamping Press delivers low-defect stamping, the LG 1370 CNC Lathe enables precise tooling, and the Semi-Automatic Tube Forming Machine ensures high-quality, scratch-free tubes with efficient part transfer.</p>
+                        <p class="mb-0">CS Metal’s advanced machines enhance manufacturing precision: the Automated Tube Forming Machine uses AI for real-time control, the 600 Ton Stamping Press delivers low-defect stamping, the LG 1370 CNC Lathe enables precise tooling, and the Semi-Automatic Tube Forming Machine ensures high-quality, scratch-free tubes with efficient part transfer.</p>
                     </div>
                 </div>
 
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pb-0">
                     <div class="product-wrap">
                         <div class="product-image-wrap">
                             <img src="./img/Solution/img_testing.jpg" alt="product 3" style="width: 100%;">
                             <a class="image-arrow" href="solutions/testing.php"><div>→</div></a>
                         </div>
                         <h3 class="mt-3">Testing</h3>
-                        <p>CS Metal’s testing solutions ensure reliability. The Metal Hardness Measurement Machine offers precise hardness testing for validation. The Push-Pull Force Test evaluates connection strength over 1500 lbs. The Transportation Compression Test simulates transit conditions, while the Transportation Vibration Test assesses resilience against vibrations.</p>
+                        <p class="mb-0">CS Metal’s testing solutions ensure reliability. The Metal Hardness Measurement Machine offers precise hardness testing for validation. The Push-Pull Force Test evaluates connection strength over 1500 lbs. The Transportation Compression Test simulates transit conditions, while the Transportation Vibration Test assesses resilience against vibrations.</p>
                     </div>
                 </div>
             </div>
@@ -92,7 +92,7 @@
             <div class="line line-left"></div>
             <div class="line line-right"></div>
             <div class="row section-padding">
-                <div class="col-12 text-center p-3">
+                <div class="col-12 text-center p-0">
                     <div class="title">Built to Perform, Designed to Last</div>
                 </div>
             </div>
