5b80157025284421881112b028e6802e135a015f
TonyHuang <hzx0109@gmail.com>
2024-12-18 15:00:16 -0800

    update responsive for inspection page

--- solutions/inspection.php
+++ solutions/inspection.php
@@ -14,16 +14,16 @@
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding">
-            <div class="col-md-8">
+            <div class="col-md-8 py-0">
                 <p class="slash-title"><span class="text-color">/ </span>Solutions</p>
                 <div class="title">Inspection</div>
             </div>
         </div>
         <div class="row mx-0 intro-container">
-            <div class="col-md-8">
-                <h3 class="solution-banner-text">CS Metal’s comprehensive inspection process integrates advanced technologies to ensure the highest quality standards throughout production. The Gom Scan facilitates early defect detection, while the Inspector CMM provides precise dimensional measurements. The Spectralight QC machine guarantees color consistency, and the QMS software offers real-time tracking and traceability. Together, these innovative tools ensure rigorous quality control from initial inspection to final production, reflecting CS Metal’s unwavering commitment to excellence in manufacturing.</h3>
+            <div class="col-md-8 py-0">
+                <h3 class="solution-banner-text mb-0">CS Metal’s comprehensive inspection process integrates advanced technologies to ensure the highest quality standards throughout production. The Gom Scan facilitates early defect detection, while the Inspector CMM provides precise dimensional measurements. The Spectralight QC machine guarantees color consistency, and the QMS software offers real-time tracking and traceability. Together, these innovative tools ensure rigorous quality control from initial inspection to final production, reflecting CS Metal’s unwavering commitment to excellence in manufacturing.</h3>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 py-0">
                 <img src="img/Solution/inspection/inspection_main.jpg" alt="Inspection Banner" class="image solution-banner scroll2moveup">
             </div>
         </div>
@@ -38,17 +38,17 @@
 
         <div class="section-padding">
             <div class="row">
-                <div class="col-sm-12 col-md-12">
+                <div class="col-sm-12 col-md-12 py-0">
                     <p class="slash-title"><span class="text-color">/ </span>GOM SCAN</p>
                     <div class="title">3D SCANNING TECHNOLOGY</div>
                 </div>
             </div>
             <div class="row padding-top-50">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 py-0">
                     <div>
                         <p>Welcome to our introduction to Gom Scan, a leading solution in 3D scanning technology, specifically designed for precision in metal stamping parts. With Gom Scan, we ensure the highest quality through rigorous first piece inspections, enabling us to detect any deviations from specifications right from the start.</p>
                         <p>As we transition into mass production, Gom Scan plays a crucial role in maintaining consistent quality control (QC), allowing for real-time monitoring and adjustment throughout the manufacturing process. This not only enhances the reliability of our products but also drives process improvement initiatives by identifying areas for optimization.</p>
-                        <p>Join us as we explore how Gom Scan is revolutionizing the metal stamping industry, ensuring accuracy and efficiency at every stage, from the first inspection to large-scale production!</p>
+                        <p class="mb-0">Join us as we explore how Gom Scan is revolutionizing the metal stamping industry, ensuring accuracy and efficiency at every stage, from the first inspection to large-scale production!</p>
                     </div>
                 </div>
 
@@ -56,7 +56,7 @@
                     <img src="img/Solution/inspection/img_inspection_1.jpg" alt="product 1" class="image scroll2movedown solutions-img1-margin">
                 </div>
 
-                <div class="col-sm-12 col-md-4 solution-image-2">
+                <div class="col-sm-12 col-md-4 solution-image-2 pb-0">
                     <img src="img/Solution/inspection/img_inspection_2.jpg" alt="product 2" class="image scroll2moveup solutions-img2-margin">
                 </div>
             </div>
@@ -84,13 +84,13 @@
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding-lg">
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pt-0">
                 <p class="mb-0">Introducing our advanced Coordinate Measuring Machine (CMM), Inspector, a state-of-the-art measuring device designed for precise dimensional inspection in manufacturing. This innovative tool features computerized movement along the X, Y, and Z axis, allowing for highly accurate measurements of both large and small inner and outer dimensions.</p>
             </div>
             <div class="col-sm-12 col-md-4">
                 <p class="mb-0">The CMM Inspector is perfect for a variety of applications, delivering reliable results across different sizes and shapes of components. Its ability to generate detailed digital reports streamlines the quality assurance process, providing manufacturers with clear documentation of measurement results for easy analysis and compliance tracking.</p>
             </div>
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pb-0">
                 <p class="mb-0">With its advanced technology and user-friendly interface, our CMM Inspector enhances productivity and ensures the highest standards of accuracy in dimensional measurement. Experience the benefits of precision measurement with our CMM Inspector, a crucial asset for maintaining product quality and integrity in any manufacturing environment!</p>
             </div>
         </div>
@@ -100,11 +100,11 @@
     <div class="black-white-background">
         <div class="container">
             <div class="row section-padding-lg text-white">
-                <div class="col-md-6">
+                <div class="col-md-6 pt-0">
                     <p class="slash-title"><span class="text-color">/ </span>Spectralight QC Light Booth</p>
                     <div class="title">COLOR QUALITY CONTROL</div>
                 </div>
-                <div class="col-md-6 ">
+                <div class="col-md-6 pb-0">
                     <p class="mb-0">Introducing the Spectralight QC Light Booth, a state-of-the-art quality control solution designed to ensure accurate color judgment in aesthetic components. A key feature of this booth is its use of controlled light temperature along with standardized light sources, including D65, CWF, and A light temperatures, which are essential for simulating natural daylight and various artificial lighting conditions.</p>
                 </div>
             </div>
@@ -120,13 +120,13 @@
             <div class="line line-1-3"></div>
             <div class="line line-2-3"></div>
             <div class="row section-padding-lg">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pt-0">
                     <p class="mb-0">The Spectralight QC Light Booth incorporates controlled light temperature management for optimal viewing, ensuring consistency and reliability in color assessment. This environment minimizes the impact of external light variables that can skew color perception, allowing for precise evaluation.</p>
                 </div>
                 <div class="col-sm-12 col-md-4">
                     <p class="mb-0">We have a single light compartment dedicated to inspecting small components to facilitate detailed analyses. For larger components and high-volume batches of colored products, a duo light booth design is provided to accommodate comprehensive evaluations without sacrificing accuracy. </p>
                 </div>
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pb-0">
                     <p class="mb-0">The Spectralight QC Light Booth compares samples to a master target, detecting color variances. This technology helps manufacturers uphold standards, reduce defects, and ensure consistency. Experience the reliability of the Spectralight QC Light Booth—essential for color quality control!</p>
                 </div>
             </div>
@@ -154,13 +154,13 @@
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding-lg">
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pt-0">
                 <p class="mb-0">The CS Metal Quality Management System (QMS) is a customized software solution designed to ensure consistent quality throughout the manufacturing process. At the core of this QMS is a paperless quality record database that streamlines data management and enhances operational efficiency.</p>
             </div>
             <div class="col-sm-12 col-md-4">
                 <p class="mb-0">Quality data is input daily from various stages, including incoming materials inspections, in-process evaluations, critical-to-quality control charts, and finished goods inspections. This software enables efficient data entry and real-time monitoring, allowing timely identification of issues during production.</p>
             </div>
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pb-0">
                 <p class="mb-0">The system facilitates comprehensive quality performance management reviews, providing insights into trends and areas for improvement. Additionally, the QMS ensures quality traceability, allowing for easy tracking of materials and components throughout the manufacturing process.</p>
             </div>
         </div>
