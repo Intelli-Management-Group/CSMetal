a700a91781e2ce765f2d0448afea8671efaf7c83
TonyHuang <hzx0109@gmail.com>
2024-12-18 16:14:38 -0800

    update responsive for machines page

--- solutions/machine.php
+++ solutions/machine.php
@@ -14,16 +14,16 @@
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding">
-            <div class="col-md-8">
+            <div class="col-md-8 py-0">
                 <p class="slash-title"><span class="text-color">/ </span>Solutions</p>
                 <div class="title">Machines</div>
             </div>
         </div>
         <div class="row mx-0 intro-container">
-            <div class="col-md-8">
-                <h3 class="solution-banner-text">CS Metal’s machine lineup offers advanced solutions for efficient, precise manufacturing. The Automated Tube Forming Machine enhances productivity and quality with real-time AI sorting. The 600 Ton Stamping Press provides powerful, low-defect stamping for large and complex parts. The LG 1370 Hartford CNC Lathe ensures precise tooling and machining versatility, while the Semi-Automatic Tube Forming Machine combines automation with flexibility, delivering scratch-free, high-quality tubes. Together, these machines elevate manufacturing efficiency and quality.</h3>
+            <div class="col-md-8 py-0">
+                <h3 class="solution-banner-text mb-0">CS Metal’s machine lineup offers advanced solutions for efficient, precise manufacturing. The Automated Tube Forming Machine enhances productivity and quality with real-time AI sorting. The 600 Ton Stamping Press provides powerful, low-defect stamping for large and complex parts. The LG 1370 Hartford CNC Lathe ensures precise tooling and machining versatility, while the Semi-Automatic Tube Forming Machine combines automation with flexibility, delivering scratch-free, high-quality tubes. Together, these machines elevate manufacturing efficiency and quality.</h3>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 py-0">
                 <img src="img/Solution/machine/machine_main.jpg" alt="Inspection Banner" class="image solution-banner scroll2moveup">
             </div>
         </div>
@@ -55,13 +55,13 @@
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding-lg">
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pt-0">
                 <p class="mb-0">Introducing our innovative Automated Metal Tube Forming Machine, designed to revolutionize the manufacturing process. With a focus on custom engineering, this machine is tailor-made for special projects, allowing for flexibility and precision in production.</p>
             </div>
             <div class="col-sm-12 col-md-4">
                 <p class="mb-0">Our advanced technology enhances quality consistency, ensuring each metal tube meets high standards and specifications. The automated system increases production capacity, enabling manufacturers to meet high demand efficiently while maintaining exceptional quality.</p>
             </div>
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pb-0">
                 <p class="mb-0">Equipped with advanced AI technology, our Automated Metal Tube Forming Machine identifies and sorts good and bad parts in real-time, enhancing quality control. Experience the future of metal tube forming, where custom solutions and superior quality come together to elevate your production capabilities.</p>
             </div>
         </div>
@@ -76,17 +76,17 @@
 
         <div class="section-padding pt-0">
             <div class="row">
-                <div class="col-sm-12 col-md-12">
+                <div class="col-sm-12 col-md-12 py-0">
                     <p class="slash-title"><span class="text-color">/ </span>600t</p>
                     <div class="title">Metal Stamping Press Machine</div>
                 </div>
             </div>
             <div class="row padding-top-50">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 py-0">
                     <div>
                         <p>Introducing our powerful 600 Ton Metal Stamping Press Machine, designed for exceptional forming capability and efficiency in the metal stamping industry. This heavy-duty press excels at producing symmetric products and can form thick materials, making it ideal for various applications. Whether working with soft or hard materials, this machine delivers outstanding performance with precision.</p>
                         <p>With its ability to produce high-volume products while maintaining a low defect rate, our stamping press achieves a Parts Per Million (PPM) rate of less than 100, ensuring reliability in every batch. Featuring a substantial tooling table size of 3200 x 1300 mm, this machine accommodates large components like cook tops and long rails. It is also well-suited for progressive dies, enabling the production of complex products requiring multiple stamping steps.</p>
-                        <p>Experience unparalleled efficiency and precision with our 600 Ton Metal Stamping Press Machine, where advanced technology meets high-performance manufacturing!</p>
+                        <p class="mb-0">Experience unparalleled efficiency and precision with our 600 Ton Metal Stamping Press Machine, where advanced technology meets high-performance manufacturing!</p>
                     </div>
                 </div>
 
@@ -94,7 +94,7 @@
                     <img src="img/Solution/machine/img_machine_1.jpg" alt="product 1" class="image scroll2movedown machine-img1-margin">
                 </div>
 
-                <div class="col-sm-12 col-md-4 solution-image-2">
+                <div class="col-sm-12 col-md-4 solution-image-2 padding-bottom-25">
                     <img src="img/Solution/machine/img_machine_2.jpg" alt="product 2" class="image scroll2moveup machine-img2-margin">
                 </div>
             </div>
@@ -105,11 +105,11 @@
     <div class="black-white-background">
         <div class="container">
             <div class="row section-padding-lg text-white">
-                <div class="col-md-6">
+                <div class="col-md-6 pt-0">
                     <p class="slash-title"><span class="text-color">/ </span>LG 1370 Hartford CNC Lathe Machine</p>
                     <div class="title">precision machining</div>
                 </div>
-                <div class="col-md-6 ">
+                <div class="col-md-6 pb-0">
                     <p class="mb-0">Introducing the LG 1370 Hartford CNC Lathe Machine, a cutting-edge solution designed for precision machining in a variety of industrial applications. This advanced CNC lathe offers exceptional performance and reliability, making it an essential tool for manufacturers looking to achieve superior quality and efficiency in their tooling making processes.</p>
                 </div>
             </div>
@@ -125,13 +125,13 @@
             <div class="line line-1-3"></div>
             <div class="line line-2-3"></div>
             <div class="row section-padding-lg">
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pt-0">
                     <p class="mb-0">The LG 1370 is equipped with a powerful spindle that delivers high-speed machining capabilities, allowing for the effective turning of various materials, including metals and plastics. Its advanced CNC control system enables precise programming and automation, ensuring consistency and repeatability in every cycle.</p>
                 </div>
                 <div class="col-sm-12 col-md-4">
                     <p class="mb-0">With a generous swing diameter and a robust bed design, the LG 1370 accommodates both large and small workpieces, making it versatile for diverse tooling projects. The machine provides excellent tool accessibility and quick changeover options, minimizing downtime and optimizing productivity.</p>
                 </div>
-                <div class="col-sm-12 col-md-4">
+                <div class="col-sm-12 col-md-4 pb-0">
                     <p class="mb-0">LG 1370’s user-friendly interface allows operators to input specifications and monitor operations. Engineered for durability and performance, this machine is essential for workshops focused on tooling and high-quality production. Experience the precision of the LG 1370 and elevate your processes! </p>
                 </div>
             </div>
@@ -159,13 +159,13 @@
         <div class="line line-2-3"></div>
         <div class="line line-right"></div>
         <div class="row section-padding-lg">
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pt-0">
                 <p class="mb-0">Introducing our Semi-Automatic Tube Forming Machine, designed to enhance efficiency and precision in tube manufacturing. This innovative machine streamlines the tube forming process making it an ideal solution for manufacturers looking to optimize production while maintaining flexibility.</p>
             </div>
             <div class="col-sm-12 col-md-4">
                 <p class="mb-0">The semi-automatic functionality allows operators to easily set parameters and make adjustments as needed, accommodating various tube sizes and shapes. This versatility ensures consistent results, whether you are working with larger industrial tubes or smaller specialized components.</p>
             </div>
-            <div class="col-sm-12 col-md-4">
+            <div class="col-sm-12 col-md-4 pb-0">
                 <p class="mb-0">The Semi-Automatic Tube Forming Machine features automatic part transfer between stations, minimizing human contact and reducing scratches for a better cosmetic finish. Experience the advantages of this machine, where increased efficiency meets high-quality output.</p>
             </div>
         </div>
